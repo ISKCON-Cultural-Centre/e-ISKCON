e91d803c734275c1a743504d9fb21218ecd4fb13
Rajendra <hairajendra@gmail.com>
2017-11-15 15:34:47 +0530

    Added Page-not-found page for catch-all situation

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,16 +4,20 @@ import { LoginComponent } from './pages/devotee/login/login.component';
 import { ResetPasswordComponent } from './pages/devotee/reset-password/reset-password.component';
 import { AboutComponent } from './pages/about/about.component';
 import { HomeComponent } from './pages/home/home.component';
+import { PageNotFoundComponent } from './pages/pageNotFound/pageNotFound.component';
+
 
 import { AuthGuard as AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full', canActivate: [AuthGuard] },
   { path: 'home',  component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'reset', component: ResetPasswordComponent },
-  { path: 'about', component: AboutComponent, canActivate: [AuthGuard] }
+  { path: 'about', component: AboutComponent, canActivate: [AuthGuard] },
+  { path: '', redirectTo: 'home', pathMatch: 'full', canActivate: [AuthGuard] },
+  { path: '**', component: PageNotFoundComponent }
 ];
+
 
 
 @NgModule({
---
+++ client/src/app/pages/pageNotFound/pageNotFound.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pageNotFound',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class PageNotFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
