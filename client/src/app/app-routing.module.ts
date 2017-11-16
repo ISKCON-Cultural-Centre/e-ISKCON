787df68a013cf1805dfb4ad4bace4a6970074e80
karunakar <karunakar1008@gmail.com>
2017-11-16 15:28:13 +0530

    Un installed bootstrap plugins

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { LoginComponent } from './login/login.component';
 import { ResetPasswordComponent } from './login/reset-password.component';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './pageNotFound.component';
-import { DashboardComponent } from '../app/components/dashboard.component';
+import { DashboardComponent } from './components/dashboard.component';
+import { DevoteeComponent} from './components/devotee.component';
 
 import { AuthGuard as AuthGuard } from './shared/services/auth.guard';
 
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'devotee', component: DevoteeComponent, canActivate: [AuthGuard] },
   { path: 'reset', component: ResetPasswordComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full', canActivate: [AuthGuard] },
   { path: '**', component: PageNotFoundComponent }
