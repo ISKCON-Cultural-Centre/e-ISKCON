dae5d38d9d58ed57047fe6625d3639466f176a59
karunakar <karunakar1008@gmail.com>
2017-11-16 12:30:11 +0530

    Changed file formt

--- client/src/app/shared/services/auth.service.ts
+++ client/src/app/shared/services/auth.service.ts
@@ -8,13 +8,13 @@ declare var Object: any;
 @Injectable()
 export class AuthService extends LoopBackAuth {
     private sessiontoken: SDKToken = new SDKToken();
-    private remembeMe : boolean = true;
+    private remembeMe: Boolean = true;
     constructor(internalStorage: InternalStorage) {
         super(internalStorage);
         this.loadFromSession();
     }
 
-    clear(): void{
+    clear(): void {
         super.clear();
         this.clearFromSession();
     }
@@ -26,22 +26,22 @@ export class AuthService extends LoopBackAuth {
         this.persist('user', super.getCurrentUserData());
     }
 
-    loadFromSession(): void{
+    loadFromSession(): void {
         this.sessiontoken.id = sessionStorage.getItem('id');
         this.sessiontoken.userId = sessionStorage.getItem('userId');
         this.sessiontoken.user = sessionStorage.getItem('user');
-        if(this.sessiontoken.id && this.sessiontoken.user && this.sessiontoken.userId){
+        if (this.sessiontoken.id && this.sessiontoken.user && this.sessiontoken.userId) {
             super.setUser(this.sessiontoken);
         }
     }
 
-    clearFromSession(): void{
+    clearFromSession(): void {
         Object.keys(this.sessiontoken).forEach(prop => sessionStorage.removeItem(prop));
         this.sessiontoken = new SDKToken();
     }
 
     persist(prop: string, value: any): void {
-        if(this.remembeMe) {
+        if (this.remembeMe) {
             super.persist(prop, value);
         } else {
             sessionStorage.setItem(prop, JSON.stringify(value));
