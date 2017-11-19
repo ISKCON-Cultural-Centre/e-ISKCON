f7d6c02e211c4b16b5186200bffd3f21d41e3e85
Rajendra <hairajendra@gmail.com>
2017-11-19 10:59:15 +0530

    few more sample-data updates

--- reference_docs/sample-data/load-data.sql
+++ reference_docs/sample-data/load-data.sql
@@ -1,13 +1,13 @@
-truncate department;
-truncate devotee;
-truncate circle;
-truncate `spiritual-level-master`;
-truncate temple;
-
-source temple.sql
-source circle.sql
-source department.sql
-source spiritual-level-master.sql
-source devotee.sql
+use icc;
+delete from department;
+delete from devotee;
+delete from circle;
+delete from `spiritual-level-master`;
+delete from temple;
 
 
+source ~/software/eISKCON/reference_docs/sample-data/temple.sql
+source ~/software/eISKCON/reference_docs/sample-data/circle.sql
+source ~/software/eISKCON/reference_docs/sample-data/spiritual-level-master.sql
+source ~/software/eISKCON/reference_docs/sample-data/devotee.sql
+source ~/software/eISKCON/reference_docs/sample-data/department.sql
