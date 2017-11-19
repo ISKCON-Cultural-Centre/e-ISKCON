5edb173988151742b308d85b6455e81a7651c6bf
Rajendra <hairajendra@gmail.com>
2017-11-19 10:51:05 +0530

    few more sample-data updates

---
+++ reference_docs/sample-data/load-data.sql
@@ -0,0 +1,13 @@
+truncate department;
+truncate devotee;
+truncate circle;
+truncate `spiritual-level-master`;
+truncate temple;
+
+source temple.sql
+source circle.sql
+source department.sql
+source spiritual-level-master.sql
+source devotee.sql
+
+
