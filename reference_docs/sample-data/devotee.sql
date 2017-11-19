6faec51509c925869b240e5350f14e1febdba87a
Rajendra <hairajendra@gmail.com>
2017-11-19 10:08:27 +0530

    sample data added

--- reference_docs/sample-data/devotee.sql
+++ reference_docs/sample-data/devotee.sql
@@ -1,3 +1,7 @@
-id,legal-name,spiritual-name,circle-id,gender,physical-address-id,electronic-address-id,shiksha-level,spiritual-level-master-id,realm,username,password,email,emailVerified,verificationToken,created-on,updated-on,created-by,updated-by
-0c59e04e-f9f0-461f-9755-608348044bd6,Krishna,"Krishna Dasa",,M,,,,913fd405-752b-4779-9ea7-80a29ce83398,,krishna,$2a$10$RzDzwukQAvEtdNCq5hW.1eNUEkAV5OB92MHyFjgKlnGwpjv24csEm,admin@iskconsouthbangalore.org,0,NULL,"2017-11-13 08:53:51","2017-11-13 08:53:51",NULL,NULL
-5915f4c8-b3a2-42c3-a772-0edda9bd385b,"Rajendra K","Rasaraj Syamasundara Dasa",,M,,,,d2d9ce87-8f1c-4848-a816-443fd92dbc8c,,rasaraj,$2a$10$efREfBpSLRdmRmfUYsyx3.PgMM0hpQsISr8ka76395h7RqsTBOJL6,hairajendra@gmail.com,0,NULL,"2017-11-13 05:13:32","2017-11-13 05:13:32",NULL,NULL
+/*
+-- Query: SELECT * FROM icc.devotee
+LIMIT 0, 10000
+
+-- Date: 2017-11-19 10:07
+*/
+INSERT INTO `devotee` (`id`,`legal-name`,`spiritual-name`,`circle-id`,`gender`,`physical-address-id`,`electronic-address-id`,`shiksha-level`,`spiritual-level-master-id`,`realm`,`username`,`password`,`email`,`emailVerified`,`verificationToken`,`created-on`,`updated-on`,`created-by`,`updated-by`) VALUES ('0c59e04e-f9f0-461f-9755-608348044bd6','Krishna','krishna','0e17c64a-cce3-11e7-9839-20cf308ebc45','M',NULL,NULL,'','913fd405-752b-4779-9ea7-80a29ce83398','','krishna','$2a$10$RzDzwukQAvEtdNCq5hW.1eNUEkAV5OB92MHyFjgKlnGwpjv24csEm','admin@iskconsouthbangalore.org',0,NULL,NULL,NULL,NULL,NULL);
