27846bd2253479b5f240106b8726df8903289312
Sumit Upadhyaya <sumitup80@gmail.com>
2018-02-04 21:10:07 +0530

    Added new Module Lookup for Master Table entry

---
+++ src/app/lookup/lookup.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LookupComponent } from './lookup.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [LookupComponent]
+})
+export class LookupModule { }
