6c5a897e507b9e9731a37212551af04c4168c769
Rajendra <hairajendra@gmail.com>
2018-02-23 14:56:01 +0530

    temp

--- src/app/devotee/devotee-profile.component.ts
+++ src/app/devotee/devotee-profile.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Devotee } from '../shared/sdk/models/Devotee';
-import { DevoteeApi } from '../shared/sdk/services/custom/Devotee';
 import { Router } from '@angular/router';
 import { SDKToken } from '../../../src/app/shared/sdk';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Observable } from 'rxjs/Observable';
 import { startWith } from 'rxjs/operators/startWith';
 import { map } from 'rxjs/operators/map';
+
+import { Devotee } from '../shared/sdk/models/Devotee';
+import { DevoteeApi } from '../shared/sdk/services/custom/Devotee';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-devotee-profile',
@@ -16,19 +18,32 @@ import { map } from 'rxjs/operators/map';
 export class DevoteeProfileComponent implements OnInit {
 
   @Input() devoteeId: String;
+  devotee: Devotee;
 
+  stateCtrl: FormControl;
+  filteredStates: Observable<Devotee[]> ;  
   submitted = false;
 
+  email = new FormControl('', [
+    Validators.required]);
+  shikshaLevel = new FormControl('', [Validators.required, Validators.email]);
+
   constructor(private devoteeApi: DevoteeApi,
-    private router: Router, private fb: FormBuilder) {
+    private router: Router, 
+    private authService: AuthService,    
+    private fb: FormBuilder) {
   }
 
   ngOnInit() {
+    //devoteeId = this.authService.getCurrentUserId;
+    console.log(this.authService.getCurrentUserId());
+    this.devoteeApi.findById<Devotee>(this.authService.getCurrentUserId())
+    .subscribe( devotee => {this.devotee = devotee; console.log(devotee);});
   }
 
 
 
   // TODO: Remove this when we're done
-  get diagnostic() { return JSON.stringify(this.model); }
+  //get diagnostic() { return JSON.stringify(this.model); }
 
 }
