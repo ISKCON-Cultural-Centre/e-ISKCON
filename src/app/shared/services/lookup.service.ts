a8d5e4048e7d9ace6cd1c3ab8f570f6ce2c96d84
Sumit Upadhyaya <sumitup80@gmail.com>
2018-02-24 17:16:52 +0530

    cleaned up lookup component and service

--- src/app/shared/services/lookup.service.ts
+++ src/app/shared/services/lookup.service.ts
@@ -1,12 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
-import { RelationshipMasterApi } from '../sdk';
-import { RelationshipMaster } from '../sdk/models/RelationshipMaster';
-import { EventMasterApi } from '../sdk';
-import { EventMaster } from '../sdk/models/EventMaster';
-import { FestivalMasterApi } from '../sdk';
-import { FestivalMaster } from '../sdk/models/FestivalMaster';
 import { LookupData } from '../services/models/lookupData';
 import { DummyService } from '../services/dummy.service';
 import { LookupTableData } from '../services/models/lookupTableData';
@@ -14,22 +8,8 @@ import { LookupTableData } from '../services/models/lookupTableData';
 @Injectable()
 export class LookupService {
 
-  constructor(private relationshipMasterApi: RelationshipMasterApi, 
-  	private eventMasterApi: EventMasterApi,
-  	private festivalMasterApi: FestivalMasterApi,
-    private dummyService:DummyService) { }
-
-  getRelationships(): Observable<RelationshipMaster[]> {
-    return this.relationshipMasterApi.find();
-      }
-
-  getEvents(): Observable<RelationshipMaster[]> {
-    return this.eventMasterApi.find();
-      }
-
-  getFestivals(): Observable<FestivalMaster[]> {
-    return this.festivalMasterApi.find();
-      }
+  constructor(private dummyService:DummyService) { } 
+  
 
   getLookupTableData():Observable<LookupTableData[]> {
     return this.dummyService.getLookupTableData();
