fa05fb4380c94cb0c20c17e4fc533a8c1960b714
hairajendra <hairajendra@gmail.com>
2018-03-06 20:46:14 +0530

    Role Management Page completd

--- src/app/organization/role.component.ts
+++ src/app/organization/role.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, ViewChild} from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import {  ServiceRole, ServiceRoleApi } from '../../../src/app/shared/sdk';
+import {  ServiceRole, ServiceRoleApi, Department, DepartmentApi } from '../../../src/app/shared/sdk';
 import {  NotificationService} from '../shared/services';
-import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { MatPaginator, MatSort, MatTableDataSource, MatSelectChange } from '@angular/material';
 
 import { InlineEditComponent } from '../shared/components/inline-edit/inline-edit.component';
 
@@ -16,15 +16,18 @@ import { } from '../shared/'
 export class RoleComponent implements OnInit {
 
   resultsLength = 0;
-  displayedColumns = ['roleName', 'roleDescription'];
+  displayedColumns = ['roleName', 'roleDescription', 'select', 'department'];
   add = false; // add new role
-  serviceRoles: ServiceRole[];
-  dataSource = new MatTableDataSource();
+  serviceRoles: ServiceRole[] = [];
+  departments: Department[] = [];
+  dataSource = new MatTableDataSource<ServiceRole>();
+  selected = '';
 
   roleForm: FormGroup;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
+
 
   applyFilter(filterValue: string) {
     filterValue = filterValue.trim(); // Remove whitespace
@@ -35,21 +38,30 @@ export class RoleComponent implements OnInit {
   constructor(
     private notificationService: NotificationService,
     private serviceRoleApi: ServiceRoleApi,
+    private departmentApi: DepartmentApi,
     private fb: FormBuilder) {
       this.createForm();
     }
 
   ngOnInit() {
     this.loadRoles();
+    this.loadDepartments();
   }
 
   createForm() {
     this.roleForm = this.fb.group({
       name: ['', Validators.required],
-      description: ['', Validators.required]
+      description: ['', Validators.required],
+      departmentId: ['', Validators.required],
     });
   }
 
+  editDepartmentField($event: MatSelectChange, role: ServiceRole ) {
+    console.log($event);
+    console.log(role);
+    this.serviceRoleApi.patchAttributes(role.id, {departmentId: $event.value})
+    .subscribe(result => this.notificationService.notificationSubject.next('"' + result.name + '" updated successfully'));
+  }
 
   editNameField(editValue: string, el: any) {
     this.serviceRoleApi.patchAttributes(el.id, {name: editValue})
@@ -68,16 +80,26 @@ export class RoleComponent implements OnInit {
   cancel() {
     this.add = false;
   }
-  
 
   addRole() {
-    console.log(this.roleForm.value);
     this.serviceRoleApi.create<ServiceRole>(this.roleForm.value)
     .subscribe(result => {
-      this.serviceRoles.push(result);
-      console.log(result);
-      // this.loadRoles();
+      //this.serviceRoles.unshift(result);
+      this.loadRoles();
       this.notificationService.notificationSubject.next('"' + result.name + '" created successfully');
+      this.add = false;
+      this.roleForm.reset();
+      }
+    );
+  }
+
+
+  deleteRole(role: ServiceRole) {
+    this.serviceRoleApi.deleteById(role.id)
+    .subscribe(result => {
+      //this.serviceRoles.push(result);
+      this.loadRoles();
+      this.notificationService.notificationSubject.next('Role ' + '"' + role.name + '" deleted successfully');
       }
     );
   }
@@ -88,6 +110,15 @@ export class RoleComponent implements OnInit {
       roles => {
         this.resultsLength = roles.length;
         this.dataSource.data = roles;
+      }
+    )
+  }
+
+  loadDepartments() {
+    this.departmentApi.find<Department>()
+    .subscribe(
+      departments => {
+        this.departments = departments;
       }
     )
   }
