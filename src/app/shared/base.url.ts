a3aa5951f895a629c03c89e370f257193be502bd
hairajendra <hairajendra@gmail.com>
2018-08-01 11:26:09 +0530

    devotee wip

--- src/app/shared/base.url.ts
+++ src/app/shared/base.url.ts
@@ -1,4 +1,4 @@
-//export const BASE_URL = 'http://192.168.1.101:3000';
-export const BASE_URL = 'http://45.62.239.22:3000'; 
+export const BASE_URL = 'http://192.168.1.101:3000';
+//export const BASE_URL = 'http://45.62.239.22:3000'; 
 // export const BASE_URL = 'http://localhost:3000';
 export const API_VERSION = 'api';
